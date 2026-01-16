456d54348f7c5085413fc89c55d05f31727e8887
missxb <13825462467@163.com>
2026-01-16 19:32:19 +0800

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+public   公共 class   类    App {App   应用程序 {
+    public   公共 static   静态 void   无效 main   主要(String   字符串[] args   arg游戏) throws InterruptedException {
+        System.out.println("Hello Jenkins!"   "Hello Jenkins!"   "Hello Jenkins!"   "Hello Jenkins!"   "Hello Jenkins!"   "Hello Jenkins!"   "Hello Jenkins!");
+        Thread.sleep(30_000);   // 30 秒
+        System.out.println("30 s over, exiting");
     }
 }
